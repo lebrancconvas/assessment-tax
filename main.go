a13c249dee719c5c19129811f1ecec4d3c09357d
lebrancconvas <lebrancconvas@gmail.com>
2024-04-25 04:24:54 +0700

    ✨ feat: connect with database

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	godotenv.Load()
+
+	host := os.Getenv("DATABASE_URL")
+	port := os.Getenv("PORT")
+	databaseName := os.Getenv("DATABASE_NAME")
+	username := os.Getenv("ADMIN_USERNAME")
+	password := os.Getenv("ADMIN_PASSWORD")
+
+	dbInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, username, password, databaseName)
+
+	db, err := sql.Open("postgres", dbInfo)
+	if err != nil {
+		log.Fatal("Cannot Connect to Database")
+	}
+
+	fmt.Println(db)
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
