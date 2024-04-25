bfd39a5c433748ac8a53dd043d3eb8508434b532
lebrancconvas <lebrancconvas@gmail.com>
2024-04-26 03:39:59 +0700

    ✨ feat: run basic for sound checking

--- server/router.go
+++ server/router.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -8,6 +10,9 @@ func NewRouter() *echo.Echo {
 	router := echo.New()
 
 	// Router
+	router.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
+	})
 
 	return router
 }
