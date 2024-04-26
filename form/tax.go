a8013788aaf053f71e2e40b2ad52a746c087338c
lebrancconvas <lebrancconvas@gmail.com>
2024-04-26 17:58:25 +0700

    ✨ feat: update tax form

--- form/tax.go
+++ form/tax.go
@@ -1,1 +1,12 @@
 package form
+
+type TaxRequest struct {
+	TotalIncome float64 `json:"totalIncome"`
+	WHT	float64 `json:"wht"`
+	Allowances []Allowance `json:"allowances"`
+}
+
+type Allowance struct {
+	AllowanceType string `json:"allowanceType"`
+	Amount float64 `json:"amount"`
+}
