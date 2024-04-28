4a837fd033d03bb23bc6e9a60caf53165854f585
lebrancconvas <lebrancconvas@gmail.com>
2024-04-28 14:36:52 +0700

    ✅ test: add test with server

--- model/tax_test.go
+++ model/tax_test.go
@@ -115,3 +115,45 @@ func TestCalculateTaxWithWHTandAllowancesServer(t *testing.T) {
 		assert.Equal(t, testResponseJSON, rec.Body.String())
 	}
 }
+
+func TestCalculateTaxWithStepResponse(t *testing.T) {
+	testRequestJSON := `{"totalIncome: 50000.0, "wht": 0.0, "allowances": [{"allowanceType": "donation", "amount": 200000.0}]`
+	testResponseJSON := `
+		{
+			"tax": 19000.0,
+			"taxLevel": [
+				{
+					"level": "0-150,000",
+					"tax": 0.0
+				},
+				{
+					"level": "150,001-500,000",
+					"tax": 19000.0
+				},
+				{
+					"level": "500,001-1,000,000",
+					"tax": 0.0
+				},
+				{
+					"level": "1,000,001-2,000,000",
+					"tax": 0.0
+				},
+				{
+					"level": "2,000,001 ขึ้นไป",
+					"tax": 0.0
+				}
+			]
+		}
+	`
+
+	router := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/tax/calculations", strings.NewReader(testRequestJSON))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := router.NewContext(req, rec)
+
+	if assert.NoError(t, controller.CalculateTaxController(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, testResponseJSON, rec.Body.String())
+	}
+}
