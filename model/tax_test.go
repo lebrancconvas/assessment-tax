966eec03a3f23ed2d29501c9609dbe95299b09ed
lebrancconvas <lebrancconvas@gmail.com>
2024-04-28 12:24:37 +0700

    ✅ test: add test case for story 02

--- model/tax_test.go
+++ model/tax_test.go
@@ -25,4 +25,22 @@ func TestCalculateTax(t *testing.T) {
 		assert.EqualValues(t, 29000.0, taxResult)
 
 	})
+
+	t.Run("Test Calculate Tax from Total Income with WHT", func(t *testing.T) {
+		request := form.RequestData{
+			TotalIncome: 50000.0,
+			WHT: 0.0,
+			Allowances: []form.Allowance{
+				{
+					AllowanceType: "donation",
+					Amount: 0.0,
+				},
+			},
+		}
+
+		taxResult, _ := CalculateTax(request)
+
+		assert.EqualValues(t, 4000.0, taxResult)
+
+	})
 }
