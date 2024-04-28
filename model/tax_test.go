e151d7615ec1517ec23da7a9024aeceb4792a651
lebrancconvas <lebrancconvas@gmail.com>
2024-04-28 14:15:01 +0700

    ✅ test: add test with server

--- model/tax_test.go
+++ model/tax_test.go
@@ -1,8 +1,13 @@
 package model
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/labstack/echo"
+	"github.com/lebrancconvas/assessment-tax/controller"
 	"github.com/lebrancconvas/assessment-tax/form"
 	"github.com/stretchr/testify/assert"
 )
@@ -43,4 +48,20 @@ func TestCalculateTax(t *testing.T) {
 		assert.EqualValues(t, 4000.0, taxResult)
 
 	})
+}
+
+func TestCalculateTaxWithWHTServer(t *testing.T) {
+	testRequestJSON := `{"totalIncome: 50000.0, "wht": 25000.0, "allowances": [{"allowanceType": "donation", "amount": 0.0}]`
+	testResponseJSON := `{tax: 4000.0}`
+
+	router := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/tax/calculations", strings.NewReader(testRequestJSON))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := router.NewContext(req, rec)
+
+	if assert.NoError(t, controller.CalculateTaxController(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, testResponseJSON, rec.Body.String())
+	}
 }
