aa87a40336cf051f1cdb339baa18758e08a927e0
lebrancconvas <lebrancconvas@gmail.com>
2024-04-28 19:12:48 +0700

    ✨ feat: update convert csv to json

--- utils/csv.go
+++ utils/csv.go
@@ -4,7 +4,14 @@ import (
 	"encoding/csv"
 	"log"
 	"os"
+	"strconv"
 )
+
+type InputData struct {
+	TotalIncome float64
+	WHT float64
+	Donation float64
+}
 
 func ExtractCSVData(path string) [][]string{
 	csvFile, err := os.Open(path)
@@ -22,4 +29,31 @@ func ExtractCSVData(path string) [][]string{
 	}
 
 	return csvData
+}
+
+func CSVtoJSON(csvData [][]string) {
+	var requestData []InputData
+
+	for _, row := range csvData {
+		totalIncome, err := strconv.ParseFloat(row[0], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		wht, err := strconv.ParseFloat(row[1], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		donation, err := strconv.ParseFloat(row[2], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		requestData = append(requestData, InputData{
+			TotalIncome: totalIncome,
+			WHT: wht,
+			Donation: donation,
+		})
+	}
 }
