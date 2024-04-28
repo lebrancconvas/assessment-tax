53c6cf5d9ac220a4f7ab3d9a55426d0cf02bf02f
lebrancconvas <lebrancconvas@gmail.com>
2024-04-28 11:26:44 +0700

    ✨ feat: add graceful shutdown for server

--- server/server.go
+++ server/server.go
@@ -1,8 +1,31 @@
 package server
 
-import "os"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func Start() {
 	router := NewRouter()
-	router.Logger.Fatal(router.Start(":" + os.Getenv("PORT")))
+	go router.Logger.Fatal(router.Start(":" + os.Getenv("PORT")))
+
+	// Graceful Shutdown
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	<- shutdown
+	fmt.Println("\nServer is shutting down...")
+
+	context, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	err := router.Shutdown(context)
+	if err != nil {
+		router.Logger.Fatal(err)
+	}
 }
+
