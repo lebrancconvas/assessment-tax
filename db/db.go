4ccbd2282a776723b7b275692c8468d701af512a
lebrancconvas <lebrancconvas@gmail.com>
2024-04-28 22:59:28 +0700

    ✨ feat: update get environments for database

--- db/db.go
+++ db/db.go
@@ -18,16 +18,16 @@ type DB struct {
 var db *gorp.DbMap
 
 func Init() {
-	host := os.Getenv("DBHOST")
+	host := os.Getenv("DATABASE_URL")
 
-	port, err := strconv.Atoi(os.Getenv("DBPORT"))
+	port, err := strconv.Atoi(os.Getenv("DATABASE_PORT"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	user := os.Getenv("DBUSER")
-	password := os.Getenv("DBPASSWORD")
-	dbname := os.Getenv("DBNAME")
+	user := os.Getenv("DATABASE_USERNAME")
+	password := os.Getenv("DATABASE_PASSWORD")
+	dbname := os.Getenv("DATABASE_NAME")
 
 	dbinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	db, err = ConnectDB(dbinfo)
